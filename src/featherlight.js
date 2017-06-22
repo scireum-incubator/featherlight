bbaf27f1f0745023db5d43594251e85b65c936b1
Marc Egenrieder <meg@scireum.de>
2017-06-22 13:33:35 +0200

    Changes size calculations so scrollbar no longer appear on big images

--- src/featherlight.js
+++ src/featherlight.js
@@ -325,8 +325,8 @@
 				/* Calculate the worst ratio so that dimensions fit */
 				 /* Note: -1 to avoid rounding errors */
 				var ratio = Math.max(
-					w  / (parseInt(this.$content.parent().css('width'),10)-1),
-					h / (parseInt(this.$content.parent().css('height'),10)-1));
+					w  / (this.$content.parent().width()-1),
+					h / (this.$content.parent().height()-1));
 				/* Resize content */
 				if (ratio > 1) {
 					ratio = h / Math.floor(h / ratio); /* Round ratio down so height calc works */
